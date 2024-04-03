e5305052b019540dd7862c31682e2d44f9bd1af6
nicktaorminaa <159719616+nicktaorminaa@users.noreply.github.com>
2024-04-03 19:54:18 -0400

    Update offline.php

--- offline.php
+++ offline.php
@@ -1,6 +1,6 @@
 <?php
 // offline.php - My Twitter Website Offline
-// Written by:  Charles Kaplan, December 2017
+// Written by:  Nicholas Taormina, December 2022
 
 	echo "<br>&nbsp;&nbsp;$desc_short is off-line for maintenance, please try again later.<br><br>"
 
